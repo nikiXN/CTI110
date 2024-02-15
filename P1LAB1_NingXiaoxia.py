5748369f0eadc58b0f4ef6895fb00d4e18ee417e
Xiaoxia Ning <112765986+nikiXN@users.noreply.github.com>
2024-02-15 17:10:17 -0500

    Add files via upload
    First submission.

---
+++ P1LAB1_NingXiaoxia.py
@@ -0,0 +1,10 @@
+ # Xiaoxia Ning
+
+ # 2/15/2024
+
+ # P1LAB1
+
+ # This program takes a first name as input and outputs a welcome message to that name.
+frist_name = input()
+print("Hey", frist_name)
+print("Welcome to zyBooks!")
